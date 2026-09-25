7cd2d2bd9782f078714ed307531a8ce4666a9e20
nothussnain <husnainabid.prince@gmail.com>
2026-09-25 15:36:35 +0500

    practice

--- example.js
+++ example.js
@@ -1,17 +1,12 @@
-function birthday(s, d, m) {
-    let count = 0;
-    let sum = [];
-    for (let i = 0; i < s.length; i++) {
-        let l = s.slice(i, m + i).reduce((pre, next) => pre + next);
-        sum.push(l);
-        if (l === d) {
-            count++;
-        }
-    }
-    return sum;
+function migratoryBirds(arr) {
+  let unique = [...new Set(arr)];
+  let count = {};
+  unique.forEach((element) => {
+    let same = arr.filter((val) => val === element);
+    count[element] = same;
+  });
+  return count;
 }
-let s = [2, 2, 1, 3, 2];
-let d = 9;
-let m = 2;
-console.log(birthday(s, d, m));
+let arr = [2, 2, 1, 1, 3];
+console.log(migratoryBirds(arr));
 export {};
--- example.ts
+++ example.ts
@@ -1,17 +1,13 @@
 export {};
-function birthday(s: number[], d: number, m: number) {
-  let count = 0;
-  let sum = [];
-  for (let i = 0; i < s.length; i++) {
-    let l = s.slice(i, m + i).reduce((pre, next) => pre + next);
-    sum.push(l);
-    if (l === d) {
-      count++;
-    }
-  }
-  return sum;
+function migratoryBirds(arr: number[]) {
+  let unique = [...new Set(arr)];
+  let count = {};
+  unique.forEach((element) => {
+    let same = arr.filter((val) => val === element);
+    count[element] = same;
+  });
+  return count;
 }
-let s: number[] = [2, 2, 1, 3, 2];
-let d: number = 9;
-let m: number = 2;
-console.log(birthday(s, d, m));
+let arr: number[] = [2, 2, 1, 1, 3];
+
+console.log(migratoryBirds(arr));
