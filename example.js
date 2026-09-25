1be1604fdf5dd75ec0be1700d36b66abe76e8d48
nothussnain <husnainabid.prince@gmail.com>
2026-09-25 18:52:34 +0500

    solve the error

--- example.js
+++ example.js
@@ -1,11 +1,12 @@
 function migratoryBirds(arr) {
-  let unique = [...new Set(arr)];
-  let count = {};
-  unique.forEach((element) => {
-    let same = arr.filter((val) => val === element);
-    count[element] = same;
-  });
-  return count;
+    let unique = [...new Set(arr)];
+    let count = {};
+    unique.forEach((element) => {
+        let same = arr.filter((val) => val === element);
+        count[element] = same.length;
+    });
+    let large = Object.entries(count).sort(([, a], [, b]) => a - b);
+    return large[0][0];
 }
 let arr = [2, 2, 1, 1, 3];
 console.log(migratoryBirds(arr));
